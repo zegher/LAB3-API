f8aa77c4287d5a82ad9d09e25922e2c3edb99979
zegher <zegherb@hotmail.com>
2023-10-10 13:54:30 +0200

    First commit

---
+++ main.js
@@ -0,0 +1,4 @@
+import './style.css'
+import App from './src/App.js'
+
+const app = new App();
---
+++ src/App.js
@@ -0,0 +1,53 @@
+export default class App{
+    constructor(){
+        navigator.geolocation.getCurrentPosition(
+            this.showPosition.bind(this), //zorgt dat deze this vastgeplakt staat aan de functie van de positie
+            this.showError
+        );
+    }
+
+    getLocation(){
+        navigator.geolocation.getCurrentPosition(this.showPosition);
+    }
+
+    showPosition(position){
+        console.log(position);
+        let x = position.coords.latitude;
+        let y = position.coords.longitude;
+        console.log(x);
+        this.getWeather(x,y);
+    }
+
+    getWeather(x,y){
+        //url: https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m&current_weather=true&forecast_days=1
+        
+        fetch(`https://api.open-meteo.com/v1/forecast?latitude=${x}&longitude=${y}&hourly=temperature_2m&current_weather=true&forecast_days=1`)
+        .then(response => response.json())
+        .then(data => {
+            document.querySelector('h2').innerHTML = data.current_weather.temperature + ' °C';
+        })
+        .catch(error => {
+            console.log(error)
+        })
+
+        //if temperature is under 25 degrees, show a message saying its cold
+
+        fetch(`https://api.open-meteo.com/v1/forecast?latitude=${x}&longitude=${y}&hourly=temperature_2m&current_weather=true&forecast_days=1`)
+        .then(response => response.json())
+        .then(data => {
+            if(data.current_weather.temperature < 25){
+                document.querySelector('h1').innerHTML = 'It is cold';
+            }        
+        })
+        .catch(error => {
+            console.log("its nothing")
+        })
+
+        
+    }
+
+
+    showError(error){
+        console.log(error);
+    }
+}
